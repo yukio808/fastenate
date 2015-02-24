5c491b8804e0e000c96160b5d88f87ba381e2410
Ray Farias <ray@rayfarias.com>
2015-02-24 10:57:33 -1000

    update (gulpfile.js): trigger livereload when css files are compiled

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var tintlr = require('tiny-lr');
+var connect = require('gulp-connect');
 
 var PathTo = {
   Sass: './sass/**/*.scss',
   PublicFolder: './public',
-  PublicCss: './public/styles'
+  PublicCss: './public/styles',
+  PublicCssFiles: './public/styles/*.css'
 };
 
 gulp.task('watch-files', function (){
   gulp.watch(PathTo.Sass, ['compile-sass']);
+  gulp.watch(PathTo.PublicCssFiles, ['html']);
 });
 
 gulp.task('compile-sass', function (){
@@ -19,4 +21,9 @@ gulp.task('compile-sass', function (){
           .pipe(gulp.dest(PathTo.PublicCss));
 });
 
-gulp.task('default', ['compile-sass', 'watch-files'])
+gulp.task('html', function (){
+  gulp.src('./public/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('default', ['compile-sass', 'watch-files']);
