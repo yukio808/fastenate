fb805ba3c5cbc4ee12da6ac143e6dcbc23bcf66b
Ray Farias <ray@rayfarias.com>
2015-02-24 13:16:02 -1000

    ready!

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,37 @@ var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 
 var PathTo = {
-  Sass: './sass/**/*.scss',
+  SassFiles: './sass/**/*.scss',
   PublicFolder: './public',
   PublicCss: './public/styles',
   PublicCssFiles: './public/styles/*.css'
 };
 
 gulp.task('watch-files', function (){
-  gulp.watch(PathTo.Sass, ['compile-sass']);
-  gulp.watch(PathTo.PublicCssFiles, ['html']);
+  gulp.watch(PathTo.SassFiles, ['compile-sass']);
+  // gulp.watch(PathTo.PublicCssFiles);
 });
 
 gulp.task('compile-sass', function (){
   return gulp
-          .src(PathTo.Sass, ['compile-sass'])
+          .src(PathTo.SassFiles, ['compile-sass'])
           .pipe(sass())
-          .pipe(gulp.dest(PathTo.PublicCss));
+          .pipe(gulp.dest(PathTo.PublicCss))
+          .pipe(connect.reload());
+
 });
 
 gulp.task('html', function (){
-  gulp.src('./public/*.html')
+  return gulp.src('./public/*.html')
     .pipe(connect.reload());
 });
 
 gulp.task('public-server', function (){
-  return connect.server({
+  connect.server({
     root: './public',
     port: 8080,
     livereload: true
   });
 });
 
-gulp.task('data-server', function (){
-  return connect.server({
-    root: './server',
-    port: 9090,
-    livereload: false
-  });
-});
-
-gulp.task('default', ['compile-sass', 'watch-files', 'public-server', 'data-server']);
+gulp.task('default', ['public-server', 'compile-sass', 'watch-files']);
